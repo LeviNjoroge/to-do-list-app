cb65f770089687db5705efe1fe97ede344e57cb2
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-17 20:07:40 +0300

    Creating a to do app using react

--- src/pages/TodoMain.jsx
+++ src/pages/TodoMain.jsx
@@ -22,17 +22,23 @@ function TodoMain(){
     }
 
     return(<>
-        <h1>My To-Do List</h1>
-        <ol>
-            {list.map((listItem, index) => <li key={index} onClick={()=>deleteItem(index)}>{listItem}</li>)}
-        </ol>
-        {/*add items*/}
-        <input type="text" placeholder='Enter item' id='addItems'/>
-        <button onClick={addItem}>Add Item</button>
-        <br />
+    <div className="listApp">
+            <h1>My To-Do List</h1>
+            <div className="listItems">
+                <ol>
+                    {list.map((listItem, index) => <li key={index} onClick={()=>deleteItem(index)}>{listItem}</li>)}
+                </ol>
+            </div>
+            {/*add items*/}
+            <input type="text" placeholder='Enter item' id='addItems'/>
+            <button onClick={addItem}>Add Item</button>
+            <br />
 
-        {/*clear list*/}
-        <button onClick={clearList}>Clear List</button>
+            {/*clear list*/}
+            <button onClick={clearList}>Clear List</button>
+            <br /><br />
+            <p>Welcome to the To-do list app, <br />To get started, tap on the <b>Clear List</b> button to get rid of all the current placeholder values <br />Add an item to your list by typing it in the input box below and clicking the <b>Add item</b> button <br />You can delete an item by simply <b>tapping on the item</b> <br />Enjoy, and stay disciplened :)</p>
+    </div>
     </>);
 }
 
